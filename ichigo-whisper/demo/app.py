8bb34b7d50e678edb67717cf3502ebca5a158cd9
tuanlda78202 <tuanleducanh78202@gmail.com>
2025-01-03 08:29:26 +0000

    fix: update port demo

--- ichigo-whisper/demo/app.py
+++ ichigo-whisper/demo/app.py
@@ -150,4 +150,4 @@ with gr.Blocks(title="Ichigo Whisper", theme="allenai/gradio-theme") as interfac
 
 if __name__ == "__main__":
     interface.queue()
-    interface.launch(server_name="0.0.0.0", server_port=7861, share=True)
+    interface.launch(server_name="0.0.0.0", server_port=7860, share=True)
